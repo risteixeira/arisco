6f2469e0e12ccfec16ac4a2894859c6e2fc99f90
Ana Rita Teixeira <rita.s_t@hotmail.com>
2018-06-22 23:30:33 +0100

    server

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const express = require('express');
 const app = express();
 
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+ 
+app.get('/', function(req,res){
+	
+	res.end("Hello Ariscos !!");
+});
  
 app.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+  console.log( "Listening on " + server_ip_address + ", port " + server_port );
 });
