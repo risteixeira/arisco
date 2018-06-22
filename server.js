55ad98a766bdb65af752ad31687853ca15a6536e
Ana Rita Teixeira <rita.s_t@hotmail.com>
2018-06-22 23:42:16 +0100

    migo

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-var http = require('http');
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 
-
+// setup ports
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
- 
-app.get('/', function(req,res){
-	
-	res.end("Hello Ariscos !!");
+
+app.get('/', function(req, res) {
+
+	res.end('Hello YouTube!');
+
 });
- 
+
+// server listens in on port
 app.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port );
+	 console.log( "Listening on " + server_ip_address + ", server_port " + server_port );
 });
