73bd0bd11935fbb92edb32ee78b59416e23e2345
Ana Rita Teixeira <rita.s_t@hotmail.com>
2018-06-23 00:26:14 +0100

    please now

--- server.js
+++ server.js
@@ -2,12 +2,23 @@ var express = require('express');
 var app = express();
 var http = require('http');
 
-// setup ports
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+ 
+app.get('/', function(req, res) {
+
+	res.end('Hello YouTube!');
+
+});
+ 
+ http.createServer(function (request, response) {
+   response.writeHead(200, {"Content-Type": "text/plain"});
+   response.end("Hello from OpenShift\n");
+ }).listen(server_port);
 
 
-http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-	response.end("Hello from OpenShift\n");
-}).listen(port);
+// server listens in on port
+app.listen(server_port, server_ip_address, function () {
+	 console.log( "Listening on " + server_ip_address + ", server_port " + server_port );
+}); 
