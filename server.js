df2a7d0e771e3ce5c9f8e382d0514a8fd23a52cb
Ana Rita Teixeira <rita.s_t@hotmail.com>
2018-06-23 00:53:42 +0100

    :(

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080); //testing
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'); //testing
 
  
-// server listens in on port
+// server listens in on port please miga
 http.createServer(app).listen(app.get('port'), app.get('ip'), function(){ //testing
   console.log('Express server listening on port ' + app.get('port'));
 });
