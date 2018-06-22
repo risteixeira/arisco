bae758f36bf21cfeb0b3d73220c8605637ba41e1
Ana Rita Teixeira <rita.s_t@hotmail.com>
2018-06-22 16:53:47 +0100

    aaaa

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
-
-
-const app = express();
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/*+json' }));
+var express = require('express'),
+    fs = require('fs'),
+    app = express();
  
-
-
-
-app.listen('3000', () => {
-	console.log('Server started on port 3000');
+var app = express();
+ 
+var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+ 
+ 
+app.get('/', function(req, res) {
+    res.send('Hello from NodeJS  at '+ new Date());
 });
-
+ 
+ 
+app.listen(8080, ip);
+ 
+module.exports = app;
