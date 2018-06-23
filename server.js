3cb8bb91be5fc2354717145b1bde0b85c0cd69bb
Ana Rita Teixeira <rita.s_t@hotmail.com>
2018-06-23 23:02:39 +0100

    go girl

--- server.js
+++ server.js
@@ -2,19 +2,17 @@ http = require('http');
 fs = require('fs');
 port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var host = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+var datahost = process.env.OPENSHIFT__DB_HOST ;
 process.env.BABEL_DISABLE_CACHE = 1;
 
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "mysql://mysql:3306/",
-  user: "userXYS",
-  password: "7UVFpPnkAyqSrt4E"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+  host: datahost,
+  user: 'userXYS',
+  password: '7UVFpPnkAyqSrt4E',
+  database: 'sampledb'
 });
 
 
