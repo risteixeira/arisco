3a5f52d760072f00804a4028b1a98b8cf8690051
Ana Rita Teixeira <rita.s_t@hotmail.com>
2018-06-23 01:27:53 +0100

    crljo

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ http = require('http');
 fs = require('fs');
 
 server = http.createServer( function(req, res) {
-	res.writeHead(200, {"Content-Type": "text/plain"});
+   res.writeHead(200, {"Content-Type": "text/plain"});
    res.end("Hello from OpenShift\n");
+
+    if (req.method == 'POST') {
+        console.log("POST");
+    }
+    else
+    {
+        console.log("GET");
+		console.log('GET received');
+		res.end("Hello from from GEy\n");
+    }
 
 });
 
