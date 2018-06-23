69991dafb293c49fc3430030ca61d2d08c166dea
Ana Rita Teixeira <rita.s_t@hotmail.com>
2018-06-23 03:24:47 +0100

    mysql

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var host = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 process.env.BABEL_DISABLE_CACHE = 1;
 
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "mysql://mysql:3306/",
+  user: "userXYS",
+  password: "7UVFpPnkAyqSrt4E"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+
 
 const express = require('express');
 const favicon = require('express-favicon');
