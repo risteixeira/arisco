257d984b89c9e2f3d7d806e26a5815e7d0ffdb42
Ana Rita Teixeira <rita.s_t@hotmail.com>
2018-06-23 01:15:31 +0100

    now dont work

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ fs = require('fs');
 server = http.createServer( function(req, res) {
 	res.writeHead(200, {"Content-Type": "text/plain"});
    res.end("Hello from OpenShift\n");
+
+    if (req.method == 'POST') {
+        console.log("POST");
+    }
+    else
+    {
+        console.log("GET");
+		console.log('GET received');
+    }
+
 });
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
