399557543fb1162c1d2036de988f883533f7c4b4
Ana Rita Teixeira <rita.s_t@hotmail.com>
2018-06-23 02:23:53 +0100

    do sun

--- server.js
+++ server.js
@@ -1,25 +1,19 @@
 http = require('http');
 fs = require('fs');
-
-server = http.createServer( function(req, res) {
-   res.writeHead(200, {"Content-Type": "text/plain"});
-   res.end("Hello from OpenShift\n");
-
-    if (req.method == 'POST') {
-        console.log("POST");
-    }
-    else
-    {
-        console.log("GET");
-		console.log('GET received');
-		res.end("Hello from from GEy\n");
-    }
-
-});
-
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var host = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 process.env.BABEL_DISABLE_CACHE = 1;
 
-server.listen(port, host);
-console.log('Listening at http://' + host + ':' + port);
+
+const express = require('express');
+const favicon = require('express-favicon');
+ 
+const app = express();
+ 
+app.use(favicon(__dirname + '/public/favicon.png'));
+ 
+// Add your routes here, etc.
+ 
+const server = app.listen(port, function(){
+    console.log('server is running at %s .', server.address().port);
+});
